3982cb4906690e4031057437c560257ab8baa703
Iván Martínez león <ivan.martinez.sge@mango.com>
2024-03-07 11:43:22 +0100

    .

--- lib/presentation/beacons/beacon_widget.dart
+++ lib/presentation/beacons/beacon_widget.dart
@@ -19,21 +19,6 @@ class _BeaconsWidgetState extends State<BeaconsWidget> {
       ),
       child: BlocBuilder<BeaconsCubit, BeaconsState>(
         builder: (context, state) {
-          print(state.toString());
-
-          final List<String> artList = [
-            "https://st.mngbcn.com/rcs/pics/static/T4/fotos/SZ/47025923_99_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T3/fotos/SZ/37019111_99_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T3/fotos/SZ/37009112_99_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T4/fotos/SZ/47026315_99_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T5/fotos/SZ/53097666_99_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T5/fotos/SZ/57033256_TM_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T6/fotos/SZ/67850051_TM_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T5/fotos/SZ/57054046_54_B4.jpg?imwidth=320",
-            "https://st.mngbcn.com/rcs/pics/static/T3/fotos/SZ/33050554_BL_B4.jpg?imwidth=320",
-            "",
-          ];
-
           if (state is BeaconsError) {
             return Center(
               child: Text(state.message),
@@ -42,15 +27,21 @@ class _BeaconsWidgetState extends State<BeaconsWidget> {
 
           if (state is BeaconsActive) {
             return ListView.builder(
-              itemCount: artList.length,
+              padding: EdgeInsets.zero,
+              itemCount: 100,
               itemBuilder: (context, index) {
-                return Row(
-                  children: [
-                    Image.network(
-                      artList[index],
-                      height: 120.0,
-                    )
-                  ],
+                return ListTile(
+                  contentPadding: EdgeInsets.zero,
+                  leading: Image.network(
+                    "https://st.mngbcn.com/rcs/pics/static/T4/fotos/SZ/47025923_99_B4.jpg?imwidth=320",
+                    height: 120.0,
+                  ),
+                  title: const Text('perndaaa 111'),
+                  subtitle: const Text('perndaaa 111'),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  ),
                 );
               },
             );
